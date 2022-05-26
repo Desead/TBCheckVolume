8f297cd976c328da3369b58c696f7226fa5f5e98
Desead <news-piter@mail.ru>
2022-05-26 12:15:14 +0300

    find big volume

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import statistics as st
+from statistics import median
 
 from tinkoff.invest import Client
 
@@ -9,39 +9,46 @@ TOKEN = Token.broker_full
 ACCOUNT_ID = AccountNum.broker
 FIGI = 'FUTSBRF06220'
 DEPTH = 50  # max = 50
-NEED_DELTA = 10  # %
+MIN_DELTA = 10  # min multiplier for find max volume
 
 
-def GetDepthMarket(client, figi, depth):
-    r = client.market_data.get_order_book(figi=figi, depth=depth)
-    ask_price = tuple(map(lambda x: Qt(x.price), r.asks))
-    ask_volume = tuple(map(lambda x: x.quantity, r.asks))
-    bid_price = tuple(map(lambda x: Qt(x.price), r.bids))
-    bid_volume = tuple(map(lambda x: x.quantity, r.bids))
+class FindBigVolumeForInstrument:
+    '''
+    Возвращаем список кортежей аск и бид с повышенными объёмами
+    формат вывода:
+        ask = [(band_num, band_price, band_volume), ]
+        bid = [(band_num, band_price, band_volume), ]
+    '''
 
-    return ask_price, ask_volume, bid_price, bid_volume
+    def __init__(self, client, figi, depth=50):
+        self.client = client
+        self.figi = figi
+        self.depth = depth
 
+    def __GetDepthMarket(self):
+        r = self.client.market_data.get_order_book(figi=self.figi, depth=self.depth)
+        self.ask_price = tuple(map(lambda x: Qt(x.price), r.asks))
+        self.ask_volume = tuple(map(lambda x: x.quantity, r.asks))
+        self.bid_price = tuple(map(lambda x: Qt(x.price), r.bids))
+        self.bid_volume = tuple(map(lambda x: x.quantity, r.bids))
 
-def CalcBigVolume(price, volume):
-    min_volime = min(volume)
-    max_volume = max(volume)
-    mean = st.mean(volume)
-    mediana = st.median(volume)
+    def FindBigVolume(self):
+        self.__GetDepthMarket()
 
-    print(min_volime)
-    print(max_volume)
-    print(mean)
-    print(mediana)
-    print(st.quantiles(volume))
-    print(st.stdev(volume))
+        min_ask_volume = median(self.ask_volume) * MIN_DELTA
+        min_bid_volume = median(self.bid_volume) * MIN_DELTA
+
+        ask = [(k, self.ask_price[k], v) for k, v in enumerate(self.ask_volume) if v >= min_ask_volume]
+        bid = [(k, self.bid_price[k], v) for k, v in enumerate(self.bid_volume) if v >= min_bid_volume]
+        return ask, bid
 
 
 def main():
     with Client(token=TOKEN) as client:
-        ask_price, ask_volume, bid_price, bid_volume = GetDepthMarket(client, FIGI, DEPTH)
-        print(ask_price)
-        print(ask_volume)
-        CalcBigVolume(ask_price, ask_volume)
+        instr = FindBigVolumeForInstrument(client, FIGI)
+        ask, bid = instr.FindBigVolume()
+        print(ask)
+        print(bid)
 
 
 if __name__ == '__main__':
